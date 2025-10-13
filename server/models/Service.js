0b2b71f778da0fb6d07a7bcbeab057ecd5e4825f
almonzer-fadl <valmonzer@proton.me>
2025-10-13 12:50:01 +0300

    Revert "wip"
    This reverts commit 37da5d6791aa7ddb9638ac4751801e339f3a9cce.

--- server/models/Service.js
+++ server/models/Service.js
@@ -9,35 +9,13 @@ const serviceSchema = new mongoose.Schema({
     type: String
   },
   category: {
-    type: String,
-    required: true
+    type: String
   },
-  fixedPrice: {
-    type: Number,
-    required: true,
-    min: 0
+  estimatedDuration: {
+    type: Number
   },
-  laborRate: {
-    type: Number,
-    required: false
-  },
-  laborHours: {
-    type: Number,
-    required: true,
-    min: 0
-  },
-  partsRequired: [{
-    partId: { type: mongoose.Schema.Types.ObjectId, ref: 'Part' },
-    quantity: { type: Number, required: true, min: 1 },
-    cost: { type: Number, required: true, min: 0 }
-  }],
-  isActive: {
-    type: Boolean,
-    default: true
-  },
-  isTemplate: {
-    type: Boolean,
-    default: false
+  defaultLaborRate: {
+    type: Number
   }
 }, {
   timestamps: true
